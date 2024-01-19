472ac15a2776d93cd64ce8d5df844da449a2dd60
MatheusAugustoBorges <matheus_augusto92@hotmail.com>
2024-01-19 15:16:28 -0300

    requirement 3 - project Progression Rule - v1.0

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -17,13 +17,25 @@ public class App {
     System.out.println("Digite a quantidade de atividades para cadastrar: ");
     int qtdAtv = Integer.parseInt(scanner.nextLine());
 
+    int[] pesos = new int[qtdAtv];
+
     for (int i = 0; i < qtdAtv; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ": ");
       String nomeAtv = scanner.nextLine();
       System.out.println("Digite o peso da atividade " + (i + 1) + ": ");
       int pesoAtv = Integer.parseInt(scanner.nextLine());
+      pesos[i] = pesoAtv;
       System.out.println("Digite a nota obtida para " + nomeAtv + ": ");
       int notaAtv = Integer.parseInt(scanner.nextLine());
+    }
+
+    int soma = 0;
+    for (int valor : pesos) {
+      soma += valor;
+    }
+
+    if (soma != 100) {
+      System.out.println("A soma dos pesos é diferente de 100!");
     }
 
     scanner.close();
