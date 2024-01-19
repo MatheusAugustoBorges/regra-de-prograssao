e1ea1a6ba8c534d1a56243861b62f140f0027d9f
MatheusAugustoBorges <matheus_augusto92@hotmail.com>
2024-01-19 17:35:03 -0300

    requirement 4 - project Progression Rule - v3.0

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,6 +1,5 @@
 package com.trybe.java.regraprogressao;
 
-import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -19,33 +18,36 @@ public class App {
     System.out.println("Digite a quantidade de atividades para cadastrar: ");
     int qtdAtv = Integer.parseInt(scanner.nextLine());
 
-    int[] pesos = new int[qtdAtv];
-    int[] pesosNotas = new int[qtdAtv];
+    double[] pesos = new double[qtdAtv];
+    // double[] notas = new double[qtdAtv];
+    double[] pesosNotas = new double[qtdAtv];
 
     for (int i = 0; i < qtdAtv; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ": ");
       String nomeAtv = scanner.nextLine();
       System.out.println("Digite o peso da atividade " + (i + 1) + ": ");
-      int pesoAtv = Integer.parseInt(scanner.nextLine());
+      double pesoAtv = Double.parseDouble(scanner.nextLine());
       pesos[i] = pesoAtv;
       System.out.println("Digite a nota obtida para " + nomeAtv + ": ");
-      int notaAtv = Integer.parseInt(scanner.nextLine());
+      double notaAtv = Double.parseDouble(scanner.nextLine());
+      // notas[i] = notaAtv;
       pesosNotas[i] = pesoAtv * notaAtv;
     }
 
-    double soma = Arrays.stream(pesos).sum();
-    int somaNotas = Arrays.stream(pesosNotas).sum();
-    double notaFinal = somaNotas / soma;
-    DecimalFormat decimalFormat = new DecimalFormat("#.0#");
-    if (soma != 100.0) {
+    double somaPesos = Arrays.stream(pesos).sum();
+    // double somaNotas = Arrays.stream(notas).sum();
+    double somaPonderada = Arrays.stream(pesosNotas).sum();
+    double notaFinal = somaPonderada / somaPesos;
+    if (somaPesos != 100.0) {
       System.out.println("A soma dos pesos é diferente de 100!");
     } else {
-      if (notaFinal >= 85) {
-        System.out.println("Parabéns! Você alcançou " +
-            decimalFormat.format(notaFinal) + "%! E temos o prazer de informar que você obteve aprovação!");
+      if (notaFinal >= 85.0) {
+        System.out.println("Parabéns! Você alcançou %.1f".formatted(notaFinal)
+            + "%! E temos o prazer de informar que você obteve aprovação!");
       } else {
-        System.out.println("Lamentamos informar que, com base na sua pontuação alcançada neste período, " +
-            decimalFormat.format(notaFinal) + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+        System.out.println("Lamentamos informar que, com base na sua "
+            + "pontuação alcançada neste período, %.1f".formatted(notaFinal)
+            + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
       }
     }
     scanner.close();
