3aba3abf59bfb74baa82ef6cedf43f7acf5d41b1
MatheusAugustoBorges <matheus_augusto92@hotmail.com>
2024-01-19 14:56:01 -0300

    requirement 1 - project Progression Rule - v1.0

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * 'App' - Projeto - Regra de Progressão.
  */
@@ -9,5 +11,20 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+    int qtdAtv = Integer.parseInt(scanner.nextLine());
+
+    for (int i = 0; i < qtdAtv; i++) {
+      System.out.println("Digite o nome da atividade " + (i + 1) + ": ");
+      String nomeAtv = scanner.nextLine();
+      System.out.println("Digite o peso da atividade " + (i + 1) + ": ");
+      int pesoAtv = Integer.parseInt(scanner.nextLine());
+    }
+
+    scanner.close();
+
   }
 }
