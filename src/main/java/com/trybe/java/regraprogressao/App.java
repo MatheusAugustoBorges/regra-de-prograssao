d5d85170c636756e216823d9d62fcba25fe01548
MatheusAugustoBorges <matheus_augusto92@hotmail.com>
2024-01-19 15:53:24 -0300

    requirement 4 - project Progression Rule - v1.0

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,6 @@
 package com.trybe.java.regraprogressao;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -19,6 +20,7 @@ public class App {
     int qtdAtv = Integer.parseInt(scanner.nextLine());
 
     int[] pesos = new int[qtdAtv];
+    int[] pesosNotas = new int[qtdAtv];
 
     for (int i = 0; i < qtdAtv; i++) {
       System.out.println("Digite o nome da atividade " + (i + 1) + ": ");
@@ -28,15 +30,22 @@ public class App {
       pesos[i] = pesoAtv;
       System.out.println("Digite a nota obtida para " + nomeAtv + ": ");
       int notaAtv = Integer.parseInt(scanner.nextLine());
+      pesosNotas[i] = pesoAtv * notaAtv;
     }
 
-    int soma = Arrays.stream(pesos).sum();
-
-    if (soma != 100) {
+    double soma = Arrays.stream(pesos).sum();
+    int somaNotas = Arrays.stream(pesosNotas).sum();
+    double notaFinal = somaNotas / soma;
+    DecimalFormat decimalFormat = new DecimalFormat("#.0#");
+    if (soma != 100.0) {
       System.out.println("A soma dos pesos é diferente de 100!");
+    } else {
+      if (notaFinal >= 85) {
+        System.out.println("Parabéns! Você alcançou " + decimalFormat.format(notaFinal) + "%! E temos o prazer de informar que você obteve aprovação!");
+      } else {
+        System.out.println("Lamentamos informar que, com base na sua pontuação alcançada neste período, " + decimalFormat.format(notaFinal) + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+      }
     }
-
     scanner.close();
-
   }
 }
