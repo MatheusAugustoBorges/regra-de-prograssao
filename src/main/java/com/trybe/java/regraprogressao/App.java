69e08ea79db3d353a039132659dfb9508cb754b1
MatheusAugustoBorges <matheus_augusto92@hotmail.com>
2024-01-19 18:13:34 -0300

    requirement 4 - project Progression Rule - v4.0

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,6 +1,7 @@
 package com.trybe.java.regraprogressao;
 
 import java.util.Arrays;
+import java.util.Locale;
 import java.util.Scanner;
 
 /**
@@ -13,13 +14,13 @@ public class App {
    */
   public static void main(String[] args) {
 
+    Locale.setDefault(Locale.US);
     Scanner scanner = new Scanner(System.in);
 
     System.out.println("Digite a quantidade de atividades para cadastrar: ");
     int qtdAtv = Integer.parseInt(scanner.nextLine());
 
     double[] pesos = new double[qtdAtv];
-    // double[] notas = new double[qtdAtv];
     double[] pesosNotas = new double[qtdAtv];
 
     for (int i = 0; i < qtdAtv; i++) {
@@ -35,7 +36,6 @@ public class App {
     }
 
     double somaPesos = Arrays.stream(pesos).sum();
-    // double somaNotas = Arrays.stream(notas).sum();
     double somaPonderada = Arrays.stream(pesosNotas).sum();
     double notaFinal = somaPonderada / somaPesos;
     if (somaPesos != 100.0) {
@@ -45,9 +45,9 @@ public class App {
         System.out.println("Parabéns! Você alcançou %.1f".formatted(notaFinal)
             + "%! E temos o prazer de informar que você obteve aprovação!");
       } else {
-        System.out.println("Lamentamos informar que, com base na sua "
-            + "pontuação alcançada neste período, %.1f".formatted(notaFinal)
-            + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+        System.out.printf("Lamentamos informar que, com base na sua "
+            + "pontuação alcançada neste período, %.1f%%"
+            + ", você não atingiu a pontuação mínima necessária para sua aprovação.", notaFinal);
       }
     }
     scanner.close();
